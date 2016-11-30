ea20c3effc191c00db9d67552968223b98d79d0c
Dillon Shu <shudillon@gmail.com>
2016-11-30 16:45:27 -0500

    Fixed code so that it worked for larger numbers

--- lists.java
+++ lists.java
@@ -2,30 +2,46 @@ public class lists
 {
 	public static void main(String[] args)
 	{
-		print2D(createArrArr(8));
+		print2D(createArrArr(34));
 		
 	}
 	public static void print2D(int[][] a)
 	{
-		int longestLength = a.length*a[0].length;
+		int longestLength = a[a.length-1][a.length-1];
+		System.out.println(a[a.length-1][a.length-1]);
 		int numberOfInts = 0;
 		int counter = 1;
+		int difference = 0;
 		String line = "";
-		while (longestLength > 1)
+		System.out.println(lengthFinder(a[0][0]));
+		while (longestLength >= 10)
 		{
 			longestLength = longestLength/10;
 			counter++;
 		}
-		for (int i = 0; i <= a.length*(counter-1) + a.length;i++)
+		System.out.println(counter);
+		for (int i = 0; i <= a.length*(counter) + a.length;i++)
 			line = line + "-";
 		System.out.println(line);
+		
 		for (int row = 0; row < a.length; row++)
 		{
 			System.out.print("|");
 			for (int column = 0; column < a[row].length; column++)
 			{
+				//System.out.print(lengthFinder(a[row][column]) + " ");
+				//System.out.print(longestLength);
 				if (lengthFinder(a[row][column])<counter)
-					System.out.print(a[row][column]+" |");
+				{
+					System.out.print(a[row][column]);
+					while (difference < counter - lengthFinder(a[row][column]))
+					{
+						System.out.print(" ");
+						difference++;
+					}
+					difference = 0;
+					System.out.print("|");
+				}
 				else
 					System.out.print(a[row][column]+"|");
 			}
@@ -36,7 +52,7 @@ public class lists
 	public static int lengthFinder(int n)
 	{
 		int counter = 1;
-		while (n >= 1)
+		while (n >= 10)
 		{
 			n = n/10;
 			counter++;
